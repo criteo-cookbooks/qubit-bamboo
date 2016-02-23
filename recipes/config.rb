7389dcde134057d04f78394f575a35808f027c94
Joris de Pooter <j.depooter@criteo.com>
2016-02-23 17:55:47 +0100

    Manage the service migration from bamboo-server to bamboo

--- recipes/config.rb
+++ recipes/config.rb
@@ -30,13 +30,17 @@ end
 service 'bamboo-server' do
   provider Chef::Provider::Service::Upstart
   action [:disable, :stop]
-  only_if { node['platform_version'].to_i < 7 }
-end 
+  only_if { node['platform_version'].to_i < 7 && ::File.exist?('/etc/init/bamboo-server.conf') }
+end
+
+file '/etc/init/bamboo-server.conf' do
+  action :delete
+end
 
 bin = ::File.join(node['qubit_bamboo']['home'], 'bamboo')
 flags = node['qubit_bamboo']['flags'].sort.map { |k, v| " -#{k}=#{v}" }.join ' '
 syslog = node['qubit_bamboo']['syslog'] ? '2>&1 | logger -p user.info -t bamboo' : ''
-bamboo_command = "#{bin} #{flags} #{syslog}"
+bamboo_command = %("#{bin} #{flags} #{syslog}")
 
 poise_service 'bamboo' do
   command bamboo_command
