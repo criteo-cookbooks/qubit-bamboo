edc06f253b397bde1949a6506f0258f504c38fb1
Joris de Pooter <j.depooter@criteo.com>
2016-02-24 19:22:27 +0100

    workaround command generation problem between upstart & systemd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.hanna@criteo.com'
 license          'Apache 2.0'
 description      'Installs/Configures qubit-bamboo'
 long_description 'Installs/Configures qubit-bamboo'
-version          '0.3.0'
+version          '0.3.1'
 supports         'centos'
 
 depends          'yum-epel'
